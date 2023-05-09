162e66db2b1a91083815602e2fd8c4ab745aacd7
artur <green.neh@gmail.com>
2023-05-09 12:00:38 +0300

    some changes map

--- src/app/user-pages/main-page/components/map-form/map-form.component.ts
+++ src/app/user-pages/main-page/components/map-form/map-form.component.ts
@@ -1,7 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { YaReadyEvent } from 'angular8-yandex-maps';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
 
 const DELIVERY_TARIFF = 25;
 const MINIMUM_COST = 1000;
@@ -11,13 +9,10 @@ const MINIMUM_COST = 1000;
   styleUrls: ['./map-form.component.css'],
 })
 export class MapFormComponent {
-  apikey = 'd9372196-27a2-4ae5-a99b-d4cc91c95fd5';
-  point!: any;
   @Input()
   total!: number;
   @Output()
   totalChange = new EventEmitter();
-  constructor(private http: HttpClient) {}
 
   // Функция, вычисляющая стоимость доставки.
   public calculate(routeLength: number): number {
@@ -58,17 +53,6 @@ export class MapFormComponent {
         const activeRoute = route.getActiveRoute();
 
         if (activeRoute) {
-          // const cord = route.getBounds();
-          // let [address1, address2] = cord;
-          // let [point4, point3] = address2;
-          // let [point2, point1] = address1;
-          // let addressFrom = point3 + ',' + point4;
-          // let addressTo = point1 + ',' + point2;
-          // console.log('from');
-          // console.log(addressFrom);
-          // console.log('to');
-          // console.log(addressTo);
-
           // Получим протяженность маршрута.
           const length = route.getActiveRoute().properties.get('distance');
           // Вычислим стоимость доставки.
@@ -89,23 +73,4 @@ export class MapFormComponent {
       });
     });
   }
-
-  // public getAddress(address: any) {
-  //   this.http
-  //     .get(
-  //       `https://geocode-maps.yandex.ru/1.x/?apikey=d9372196-27a2-4ae5-a99b-d4cc91c95fd5&geocode=37.597576,55.771899`
-  //     )
-  //     .pipe(
-  //       map((res: any) => {
-  //         this.point = Object.keys(res).map(
-  //           (key) =>
-  //             ({
-  //               ...res[key],
-  //               text: key,
-  //             } as string)
-  //         );
-  //       })
-  //     );
-  //   console.log(this.point);
-  // }
 }
